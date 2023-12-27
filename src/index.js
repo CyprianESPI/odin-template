0ba49507e683c98b3533ce1911ae41858192bc78
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-27 13:43:30 +0100

    Header+Footer template

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import Styles from "./styles.css";
 
-console.log("Todo...");
-console.log("Provide a title");
+// ================= //
+// DOM interaction
+// ================= //
+const MAIN = document.querySelector("main");
 
-alert("Follow instructions in the console");
+// ================= //
+// Main
+// ================= //
+function main() {
+    console.log("Initialazing...");
+
+    // Get url param location
+    const params = new URLSearchParams(document.location.search);
+    console.log(params);
+
+    console.log("Initialazed!");
+}
+
+main();
